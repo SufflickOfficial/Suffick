273fbb9bebb24556967ded9d73de21970baf8d3e
SufflickOfficial <sufflickofficial@gmail.com>
2025-03-30 20:26:40 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
